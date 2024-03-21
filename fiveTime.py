002cc4cc2254aa85fa6dcd4761b293934f7e8791
Yotoko <vhgcuong95@gmail.com>
2024-03-21 17:23:31 +0700

    Update PEP 8

---
+++ fiveTime.py
@@ -0,0 +1,11 @@
+print(f'My name is ')
+i = 0
+while i < 5:
+    print(f'Jimmy Five Times ( {str(i)} )')
+    i = i + 1
+
+for i in range(12, 16):
+    print(i)
+
+for i in range(0, 10, 2):
+    print(i)
