d5ab45f26285f6a85529e6ece1b91e10ff547c98
Yotoko <vhgcuong95@gmail.com>
2024-03-22 11:27:18 +0700

    zigzac

--- rust_learn_more_the_hard_way/src/main.rs
+++ rust_learn_more_the_hard_way/src/main.rs
@@ -1,3 +1,36 @@
+use std::{thread, time};
+use std::sync::atomic::{AtomicBool, Ordering};
+use std::sync::Arc;
+
 fn main() {
-    println!("Hello, world!");
+    let running = Arc::new(AtomicBool::new(true));
+    let r = running.clone();
+
+    ctrlc::set_handler(move || {
+        r.store(false, Ordering::SeqCst);
+    }).expect("Error setting Ctrl-C handler");
+
+    let mut indent = 0;
+    let mut indent_increasing = true;
+
+    println!("Waiting for Ctrl-C...");
+    while running.load(Ordering::SeqCst) {
+        let text = format!("{:>width$}********", "", width = indent);
+        println!("{}", text);
+        thread::sleep(time::Duration::from_millis(1_00));
+
+        if indent_increasing == true {
+            indent += 1;
+            if indent == 20 {
+                indent_increasing = false
+            }
+        } else {
+            indent -= 1;
+            if indent == 0 {
+                indent_increasing = true
+            }
+        }
+    }
+    println!("Got it! Exiting...");
+
 }
