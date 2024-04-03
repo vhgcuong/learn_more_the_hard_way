c584512ca778f73426affd7f6cb56ca71fa91ca2
Yotoko <vhgcuong95@gmail.com>
2024-04-03 17:29:43 +0700

    Update create orc with param in avgr

--- python_learn_more_the_hard_way/create_file/create_orc.py
+++ python_learn_more_the_hard_way/create_file/create_orc.py
@@ -1,8 +1,12 @@
-import pandas as pd
 import pyarrow as pa
 import pyarrow.orc as orc
 import gzip
+import io
+import sys
 
+filename = sys.argv[1]
+
+# Define the schema
 schema = pa.schema([
     ('camp_id', pa.string()),
     ('camp_name', pa.string()),
@@ -14,38 +18,24 @@ schema = pa.schema([
     ('lon', pa.float64())
 ])
 
+# Create a sample table
 data = {
-    'camp_id': ["1", "2", "3", "4", "5"],
-    'camp_name': ["camp1", "camp2", "camp3", "camp4", "camp5"],
-    'type': ["a", "b", "c", "d", "e"],
-    'sec': ["1", "2", "3", "4", "5"],
-    'user_id': ["user1", "user2", "user3", "user4", "user5"],
-    'client_id': ["client1", "client2", "client3", "client4", "client5"],
-    'lat': [35.900242850000000, 35.900242850000000, 35.900242850000000, 35.900242850000000, 35.900242850000000],
-    'lon': [139.897993400000020, 139.897993400000020, 139.897993400000020, 139.897993400000020, 139.897993400000020]
+    'camp_id': [sys.argv[2]],
+    'camp_name': [sys.argv[3]],
+    'type': [sys.argv[4]],
+    'sec': [sys.argv[5]],
+    'user_id': [sys.argv[6]],
+    'client_id': [sys.argv[7]],
+    'lat': [float(sys.argv[8])],
+    'lon': [float(sys.argv[9])]
 }
 
-# Create DataFrame
-df = pd.DataFrame(data)
+table = pa.Table.from_pydict(data, schema=schema)
 
-# Convert DataFrame to Arrow table
-table = pa.Table.from_pandas(df, schema=schema)
+buffer = io.BytesIO()
+with gzip.GzipFile(fileobj=buffer, mode='wb') as gzip_file:
+    orc.write_table(table, gzip_file)
 
-# Write data to Orc file
-with pa.OSFile('tmp/data.orc', 'wb') as file:
-    orc.write_table(table, file)
-
-
-#### gzip
-#################################################################################
-# Read the Orc file
-with open('tmp/data.orc', 'rb') as orc_file:
-    orc_data = orc_file.read()
-
-# Compress the Orc data using gzip
-compressed_data = gzip.compress(orc_data)
-
-# Write the compressed data to a gzip file
-with gzip.open('tmp/data.orc.gz', 'wb') as gzip_file:
-    gzip_file.write(compressed_data)
-
+# Lưu nội dung từ bộ đệm vào tệp tin
+with open(f'tmp/{filename}.orc.gz', 'wb') as file:
+    file.write(buffer.getvalue())
