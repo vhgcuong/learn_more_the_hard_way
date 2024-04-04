a6177427711c70d638ebc21b75734d70490d41f3
Yotoko <vhgcuong95@gmail.com>
2024-04-04 17:33:22 +0700

    Struct CoinFlipStreak

--- rust_learn_more_the_hard_way/src/coin_flip_streaks/coin.rs
+++ rust_learn_more_the_hard_way/src/coin_flip_streaks/coin.rs
@@ -1,12 +1,26 @@
 use rand::{thread_rng, Rng};
 
-pub fn generate(number: u32) -> Vec<String> {
-    let mut coin_flip_streaks: Vec<String> = vec![];
+pub struct CoinFlipStreak {
+    pub number: u8,
+    pub slice: u8,
+}
+
+impl CoinFlipStreak {
+    fn generate() {
+
+    }
+    fn probability_of_occurrence() {
+
+    }
+}
+
+pub fn generate(number: u32) -> String {
+    let mut coin_flip_streaks= String::new();
     for _ in 0..number {
         let mut rng = thread_rng();
         match rng.gen_range(0..=1) {
-            1 => coin_flip_streaks.push(String::from("T")),
-            _ => coin_flip_streaks.push(String::from("H")),
+            1 => coin_flip_streaks += "T",
+            _ => coin_flip_streaks += "H",
         }
     }
 
@@ -16,24 +30,29 @@ pub fn generate(number: u32) -> Vec<String> {
 pub fn consecutive_probabilities() {
     let number = 100;
     let slice = 6;
-    let amount = 10_000;
+    let amount = 10;
     let mut quantity = 0;
 
     let mut count_sub = 0;
 
     for _ in 0..amount {
         let result = generate(number);
-        result.windows(slice).for_each(|sub| {
-            let all_h = sub.iter().all(|chr| chr == "H");
-            let all_t = sub.iter().all(|chr| chr == "T" );
 
-            if all_h || all_t {
-                quantity += 1;
-                println!("{:?}", sub);
-            }
+        result
+            .chars()
+            .collect::<Vec<char>>()
+            .windows(slice)
+            .for_each(|sub| {
+                let all_h = sub.iter().all(|&chr| chr == 'H');
+                let all_t = sub.iter().all(|&chr| chr == 'T' );
 
-            count_sub += 1;
-        });
+                if all_h || all_t {
+                    quantity += 1;
+                    println!("{:?}", sub);
+                }
+
+                count_sub += 1;
+            });
     }
 
     println!("Không gian lặp: {amount}");
