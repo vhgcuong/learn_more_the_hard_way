111fe42d783faa9f906e98edf01cccb0ba1ff799
Yotoko <vhgcuong95@gmail.com>
2024-04-04 14:54:48 +0700

    Update gitignore

--- python_learn_more_the_hard_way/create_file/create_orc.py
+++ python_learn_more_the_hard_way/create_file/create_orc.py
@@ -1,41 +1,33 @@
 import pyarrow as pa
 import pyarrow.orc as orc
-import gzip
-import io
 import sys
 
 filename = sys.argv[1]
 
 # Define the schema
+# Define the schema
 schema = pa.schema([
-    ('camp_id', pa.string()),
-    ('camp_name', pa.string()),
-    ('type', pa.string()),
-    ('sec', pa.string()),
+    ('campaign_id', pa.int64()),
+    ('campaign_name', pa.string()),
+    ('action_type', pa.string()),
+    ('timestamps', pa.int64()),
     ('user_id', pa.string()),
-    ('client_id', pa.string()),
     ('lat', pa.float64()),
     ('lon', pa.float64())
 ])
 
-# Create a sample table
+# data
 data = {
-    'camp_id': [sys.argv[2]],
-    'camp_name': [sys.argv[3]],
-    'type': [sys.argv[4]],
-    'sec': [sys.argv[5]],
+    'campaign_id': [int(sys.argv[2])],
+    'campaign_name': [sys.argv[3]],
+    'action_type': [sys.argv[4]],
+    'timestamps': [int(sys.argv[5])],
     'user_id': [sys.argv[6]],
-    'client_id': [sys.argv[7]],
-    'lat': [float(sys.argv[8])],
-    'lon': [float(sys.argv[9])]
+    'lat': [float(sys.argv[7])],
+    'lon': [float(sys.argv[8])]
 }
 
 table = pa.Table.from_pydict(data, schema=schema)
 
-buffer = io.BytesIO()
-with gzip.GzipFile(fileobj=buffer, mode='wb') as gzip_file:
-    orc.write_table(table, gzip_file)
-
-# Lưu nội dung từ bộ đệm vào tệp tin
-with open(f'tmp/{filename}.orc.gz', 'wb') as file:
-    file.write(buffer.getvalue())
+with pa.OSFile(f'tmp/{filename}.orc.snappy', 'wb') as file:
+    orc.write_table(table, file, compression='snappy')
